71328af282c5d8904cdca2b777e187fd86f749dc
wlkguedes <wilkie.guedes@gmail.com>
2015-05-03 22:24:27 -0300

    CategoriaCadastradaException.java

---
+++ ControleDeQualidade/src/unibratec/controlequalidade/exceptions/CategoriaCadastradaException.java
@@ -0,0 +1,12 @@
+package unibratec.controlequalidade.exceptions;
+
+public class CategoriaCadastradaException extends Exception{
+		
+	public CategoriaCadastradaException() {
+		super();
+	}
+	
+	public CategoriaCadastradaException(String msg) {
+		super(msg);
+	}
+}
